5e8499f367c3b6fdac472a0dd12fcc2dfe8d6124
Mohan <mohansisodiya50@gmail.com>
2020-08-24 12:46:39 +0530

    Take 2 for github pages

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Items from './pages/Items/Items';
 import Deals from './pages/Deals/Deals';
 import Customers from './pages/Customers/Customers';
 import General from './pages/General/General';
 
 const Routes = () => (
-	<BrowserRouter  basename={process.env.PUBLIC_URL}>
-		<Switch>
-			<Route path="/items">
-				<Items />
-			</Route>
-			<Route path="/deals">
-				<Deals />
-			</Route>
-			<Route path="/general">
-				<General />
-			</Route>
-			<Route path="/customers">
-				<Customers />
-			</Route>
-			<Route path="/">
-				<General />
-			</Route>
-		</Switch>
-	</BrowserRouter>
+	<Switch>
+		<Route path={process.env.PUBLIC_URL + '/items'}>
+			<Items />
+		</Route>
+		<Route path={process.env.PUBLIC_URL + '/deals'}>
+			<Deals />
+		</Route>
+		<Route path={process.env.PUBLIC_URL + '/general'}>
+			<General />
+		</Route>
+		<Route path={process.env.PUBLIC_URL + '/customers'}>
+			<Customers />
+		</Route>
+		<Route path="/">
+			<General />
+		</Route>
+	</Switch>
 );
 
 export default Routes;
