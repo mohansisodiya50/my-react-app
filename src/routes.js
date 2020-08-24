091ceedb39f77d9937d52534f8a3646c3dd82032
Mohan <mohansisodiya50@gmail.com>
2020-08-24 12:33:35 +0530

    Fix for github pages error

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Items from './pages/Items/Items';
 import Deals from './pages/Deals/Deals';
 import Customers from './pages/Customers/Customers';
 import General from './pages/General/General';
 
 const Routes = () => (
-  <Switch>
-    <Route path="/items">
-      <Items />
-    </Route>
-    <Route path="/deals">
-      <Deals />
-    </Route>
-    <Route path="/general">
-      <General />
-    </Route>
-    <Route path="/customers">
-      <Customers />
-    </Route>
-    <Route path="/">
-      <General />
-    </Route>
-  </Switch>
+	<BrowserRouter  basename={process.env.PUBLIC_URL}>
+		<Switch>
+			<Route path="/items">
+				<Items />
+			</Route>
+			<Route path="/deals">
+				<Deals />
+			</Route>
+			<Route path="/general">
+				<General />
+			</Route>
+			<Route path="/customers">
+				<Customers />
+			</Route>
+			<Route path="/">
+				<General />
+			</Route>
+		</Switch>
+	</BrowserRouter>
 );
 
 export default Routes;
